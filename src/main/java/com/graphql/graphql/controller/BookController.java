a3b4902ceef9512f5b8654f937c8ce7ffba5fd43
Calfus - Kunal Avghade <calfus-kunalavghade@Calfuss-MacBook-Pro.local>
2023-09-17 13:29:23 +0530

    basic graphql query and mutation

--- src/main/java/com/graphql/graphql/controller/BookController.java
+++ src/main/java/com/graphql/graphql/controller/BookController.java
@@ -1,31 +1,42 @@
 package com.graphql.graphql.controller;
 
 import com.graphql.graphql.entity.Book;
+import com.graphql.graphql.graphql.BookInput;
 import com.graphql.graphql.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/book")
+
+@Controller
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    @PostMapping
-    public Book create(@RequestBody Book book) {
-        return  bookService.create(book);
+    @MutationMapping("create")
+    public Book create(@Argument BookInput book) {
+        Book bookObj = new Book();
+        bookObj.setPrice(book.getPrice());
+        bookObj.setDesc(book.getDesc());
+        bookObj.setAuthor(book.getAuthor());
+        bookObj.setPages(book.getPages());
+        bookObj.setTitle(book.getTitle());
+        return  bookService.create(bookObj);
     }
 
-    @GetMapping
+    @QueryMapping("allBooks")
     public List<Book> getAll() {
         return bookService.getAll();
     }
 
-    @GetMapping("/{bookId}")
-    public Book get(@PathVariable int bookId) {
+    @QueryMapping("getBook")
+    public Book get(@Argument int bookId) {
         return bookService.get(bookId );
     }
 }
+
---
+++ src/main/java/com/graphql/graphql/graphql/BookInput.java
@@ -0,0 +1,49 @@
+package com.graphql.graphql.graphql;
+
+public  class BookInput {
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+}
